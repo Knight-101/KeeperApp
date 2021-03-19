4573c0f5d4e3f9d583082094d658157e38fd15b6
Sanskar Mohapatra <sanskarmohapatra@gmail.com>
2021-03-19 13:06:48 +0530

    fixed some bugs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,8 +12,8 @@ function Home() {
     <main class="px-4" style={{margin:"8rem",textAlign:"center",lineHeight:"4.5rem"}} >
       <h1>Wanna keep your notes safe?</h1>
       <p class="lead">Then go ahead, register yourself and start making notes.</p>
-      <a href="/Register"><button style={{backgroundColor: '#f5ba13',color:"white",fontSize:"1.8rem",borderRadius:"20px",margin:"0 3rem 0 0"}} class="w-25 btn btn-sm" type="submit">Register</button></a>
-      <a href="/Signin"><button style={{backgroundColor: '#f5ba13',color:"white",fontSize:"1.8rem",borderRadius:"20px"}} class="w-25 btn btn-sm" type="submit">Sign in</button></a>
+      <a href="/register"><button style={{backgroundColor: '#f5ba13',color:"white",fontSize:"1.8rem",borderRadius:"20px",margin:"0 3rem 0 0"}} class="w-25 btn btn-sm" type="submit">Register</button></a>
+      <a href="/signin"><button style={{backgroundColor: '#f5ba13',color:"white",fontSize:"1.8rem",borderRadius:"20px"}} class="w-25 btn btn-sm" type="submit">Sign in</button></a>
     </main>
     
     </div>
